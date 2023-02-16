66ed33e76c1ec70ba03d80ab292d40ae5e256e76
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-02-16 16:58:48 +0500

    Linters Fixed

--- src/Component/Calculator.js
+++ src/Component/Calculator.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../Logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 const Calculator = () => {
-   const [calcData, setCalcData] = useState ({
+  const [calcData, setCalcData] = useState({
     total: null,
     next: null,
     operation: null,
-  }); 
-  
+  });
 
   const calculateHandling = (e) => {
     const buttonName = e.target.name;
@@ -17,36 +15,35 @@ const Calculator = () => {
     return setCalcData((obj) => calculate(obj, buttonName));
   };
 
-    const { total, next, operation } = calcData;
-    const outputNumber = `${total}${operation}${next}`.replace(/null/g, '').replace(/undefined/g, '');
+  const { total, next, operation } = calcData;
+  const outputNumber = `${total}${operation}${next}`.replace(/null/g, '').replace(/undefined/g, '');
 
-    return (
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="current-operand">{output === '' ? '0' : output}</div>
-        </div>
-        <button type="button" name="AC" onClick={this.calculateHandling}>AC</button>
-        <button type="button" name="+/-" onClick={this.calculateHandling}>+/-</button>
-        <button type="button" name="%" onClick={this.calculateHandling}>%</button>
-        <button type="button" className="opration-symbol" name="&divide;" onClick={this.calculateHandling}>&divide;</button>
-        <button type="button" name="7" onClick={this.calculateHandling}>7</button>
-        <button type="button" name="8" onClick={this.calculateHandling}>8</button>
-        <button type="button" name="9" onClick={this.calculateHandling}>9</button>
-        <button type="button" className="opration-symbol" name="*" onClick={this.calculateHandling}>*</button>
-        <button type="button" name="4" onClick={this.calculateHandling}>4</button>
-        <button type="button" name="5" onClick={this.calculateHandling}>5</button>
-        <button type="button" name="6" onClick={this.calculateHandling}>6</button>
-        <button type="button" className="opration-symbol" name="-" onClick={this.calculateHandling}>-</button>
-        <button type="button" name="1" onClick={this.calculateHandling}>1</button>
-        <button type="button" name="2" onClick={this.calculateHandling}>2</button>
-        <button type="button" name="3" onClick={this.calculateHandling}>3</button>
-        <button type="button" className="opration-symbol" name="+" onClick={this.calculateHandling}>+</button>
-        <button type="button" className="span-two" name="0" onClick={this.calculateHandling}>0</button>
-        <button type="button" name="." onClick={this.calculateHandling}>.</button>
-        <button type="button" className="opration-symbol" name="=" onClick={this.calculateHandling}>=</button>
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="current-operand">{outputNumber === '' ? '0' : outputNumber}</div>
       </div>
-    );
-  };
-
+      <button type="button" name="AC" onClick={calculateHandling}>AC</button>
+      <button type="button" name="+/-" onClick={calculateHandling}>+/-</button>
+      <button type="button" name="%" onClick={calculateHandling}>%</button>
+      <button type="button" className="opration-symbol" name="&divide;" onClick={calculateHandling}>&divide;</button>
+      <button type="button" name="7" onClick={calculateHandling}>7</button>
+      <button type="button" name="8" onClick={calculateHandling}>8</button>
+      <button type="button" name="9" onClick={calculateHandling}>9</button>
+      <button type="button" className="opration-symbol" name="*" onClick={calculateHandling}>*</button>
+      <button type="button" name="4" onClick={calculateHandling}>4</button>
+      <button type="button" name="5" onClick={calculateHandling}>5</button>
+      <button type="button" name="6" onClick={calculateHandling}>6</button>
+      <button type="button" className="opration-symbol" name="-" onClick={calculateHandling}>-</button>
+      <button type="button" name="1" onClick={calculateHandling}>1</button>
+      <button type="button" name="2" onClick={calculateHandling}>2</button>
+      <button type="button" name="3" onClick={calculateHandling}>3</button>
+      <button type="button" className="opration-symbol" name="+" onClick={calculateHandling}>+</button>
+      <button type="button" className="span-two" name="0" onClick={calculateHandling}>0</button>
+      <button type="button" name="." onClick={calculateHandling}>.</button>
+      <button type="button" className="opration-symbol" name="=" onClick={calculateHandling}>=</button>
+    </div>
+  );
+};
 
 export default Calculator;
